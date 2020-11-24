295dbef8bf1df159f3e9f271afb3bbfe853da6c6
Ian Gerolamo <iangerolamo@gmail.com>
2020-11-23 21:51:18 -0300

    New changes

---
+++ src/pages/RecyclesMap/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const RecyclesMap: React.FC = () => <h1>RecyclesMap</h1>;
+
+export default RecyclesMap;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Dashboard from '../pages/Dashboard';
-import Repository from '../pages/Repository';
+import Landing from '../pages/Landing';
+import RecyclesMap from '../pages/RecyclesMap';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={Dashboard} />
-    <Route path="/repository" component={Repository} />
+    <Route path="/" exact component={Landing} />
+    <Route path="/recyclesmap" component={RecyclesMap} />
   </Switch>
 );
 
